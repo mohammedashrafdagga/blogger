86d259c88ccc19c8a2a0c1919f093d29c181606c
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-01-28 21:20:29 +0200

    build homepage with testing and post and tag model

--- blogger/settings/base.py
+++ blogger/settings/base.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blogger.blog",
+    "django_htmx",
+    "taggit",
 ]
 
 MIDDLEWARE = [
@@ -36,6 +38,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "blogger.urls"
@@ -43,7 +46,7 @@ ROOT_URLCONF = "blogger.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -52,6 +55,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": ["blogger.blog.templatetags.tag_cloud"],
         },
     },
 ]
@@ -102,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- blogger/tests/blog/test_models.py
+++ blogger/tests/blog/test_models.py
@@ -10,3 +10,7 @@ class TestPostModel:
     def test_str_return(self, post_factory):
         post = post_factory(title=self.post_title)
         assert post.__str__() == self.post_title
+
+    def test_add_tag(self, post_factory):
+        x = post_factory(title=self.post_title, tags=["test-tags"])
+        assert x.tags.count() == 1
